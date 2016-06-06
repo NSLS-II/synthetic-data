7dd126518ecd27c20043332f299d15390bd6c300
danielballan <daniel.b.allan@gmail.com>
2016-06-06 17:46:48 -0400

    return uids

--- synthetic_data/basic.py
+++ synthetic_data/basic.py
@@ -5,31 +5,31 @@ from .utils import RE_with_mds
 
 def simple_count(mds):
     RE = RE_with_mds(mds)
-    RE(count([det]))
+    return RE(count([det]))
 
 
 def multi_count(mds):
     RE = RE_with_mds(mds)
-    RE(count([det], num=3))
+    return RE(count([det], num=3))
 
 
 def step_scan(mds):
     RE = RE_with_mds(mds)
-    RE(scan([det], motor, 1, 5, 5))
+    return RE(scan([det], motor, 1, 5, 5))
 
 
 def step_scan_with_overlapping_baseline(mds):
     "same det in 'baseline' and in 'primary' event stream"
     RE = RE_with_mds(mds)
-    RE(baseline_wrapper(scan([det], motor, 1, 5, 5), [det]))
+    return RE(baseline_wrapper(scan([det], motor, 1, 5, 5), [det]))
 
 
 def step_scan_with_distinct_baseline(mds):
     "'baseline' and 'primary' event stream use different dets"
     RE = RE_with_mds(mds)
-    RE(baseline_wrapper(scan([det], motor, 1, 5, 5), [det1]))
+    return RE(baseline_wrapper(scan([det], motor, 1, 5, 5), [det1]))
 
 
 def fly_scan(mds):
     RE = RE_with_mds(mds)
-    RE(fly([flyer]))
+    return RE(fly([flyer]))
